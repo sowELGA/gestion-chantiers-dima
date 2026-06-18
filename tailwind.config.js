f0b18d23e451458e2a55856edc86b324bf45c596
ELGA <algassimousow799@gmail.com>
2026-06-18 01:26:13 +0100

    Initial commit - Setup Laravel + Tailwind CSS

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+import defaultTheme from "tailwindcss/defaultTheme";
+import forms from "@tailwindcss/forms";
+
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: ["./resources/**/*.blade.php", "./resources/**/*.js"],
+    theme: {
+        extend: {
+            colors: {
+                primary: "var(--color-primary)",
+                secondary: "var(--color-secondary)",
+                accent: "var(--color-accent)",
+                lightbg: "var(--color-lightbg)",
+                muted: "var(--color-muted)",
+            },
+        },
+    },
+    plugins: [],
+};
